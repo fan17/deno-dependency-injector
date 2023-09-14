7bf62a65ef489d18e369c4be05be54953271291a
Andrzej Jedrzejczak <jedrzejczak.andrzej89@gmail.com>
2023-09-14 12:48:28 +0200

    fix: get reflect from deno_reflect

--- injector.ts
+++ injector.ts
@@ -1,4 +1,4 @@
-import { Reflect } from "https://deno.land/x/reflect_metadata@v0.1.12-2/mod.ts";
+export { Reflect } from "https://deno.land/x/deno_reflect@v0.2.1/mod.ts";
 import type { Constructor } from "./helper_types.ts";
 
 export interface InjectionMetadata {
